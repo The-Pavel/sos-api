1441da6e59b83c61a0cf65c96c494b1b74997b00
Nini Zhu <pn.zhu76@gmail.com>
2019-06-15 13:25:35 +0800

    set up CRUD for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,42 @@
 class PostsController < ApplicationController
+  before_action set_post, only: [:show]
+
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      render :show, status: :created
+    else
+      render_error
+    end
   end
 
   def update
+    if @post.update(post_params)
+      render :show
+    else
+      render_error
+    end
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def destroy
+    @post.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:description, :capacity, :location, :contact_number, :is_full, :language, :user_id)
   end
 end
