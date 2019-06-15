5c4c4045955eed737fc2e5b424bc54275943a90e
Pavel Liser <liser.pavel@gmail.com>
2019-06-15 20:13:14 +0800

    cleaned seeds, added comment translation logic to show, updated jbuilder with lat long

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,10 +16,15 @@ puts "cleared"
 3.times do
   puts 'creating user - post - comment pair'
   user = User.create(nickname: Faker::Name.name, contact_number: Faker::PhoneNumber.cell_phone, address: Faker::Address.full_address, language: ['zh', 'en'].sample)
-  post = Post.create(description: Faker::Quote.famous_last_words, capacity: (2..20).to_a.sample, location: Faker::Address.full_address, contact_number: Faker::PhoneNumber.cell_phone, is_full: [true, false].sample, language: ['zh', 'en'].sample, user: user)
-  Comment.create(nickname: Faker::Name.name, comment: Faker::Quote.famous_last_words, language: ['zh', 'en'].sample, post: post)
+  # post = Post.create(description: Faker::Quote.famous_last_words, capacity: (2..20).to_a.sample, location: Faker::Address.full_address, contact_number: Faker::PhoneNumber.cell_phone, is_full: [true, false].sample, language: ['zh', 'en'].sample, user: user)
+  # Comment.create(nickname: Faker::Name.name, comment: Faker::Quote.famous_last_words, language: ['zh', 'en'].sample, post: post)
 end
 
-Post.create(description: "你好，你过来吧！", capacity: (2..20).to_a.sample, location: Faker::Address.full_address, contact_number: Faker::PhoneNumber.cell_phone, is_full: [true, false].sample, language: 'zh', user: User.first)
-Post.create(description: "Shanghai", capacity: (2..20).to_a.sample, location: "Shanghai Pearl Tower", contact_number: Faker::PhoneNumber.cell_phone, is_full: [true, false].sample, language: 'en', user: User.first, lat: "31.240718842", long: "121.501045227")
-Post.create(description: "Beijing", capacity: (2..20).to_a.sample, location: "Tian An Men", contact_number: Faker::PhoneNumber.cell_phone, is_full: [true, false].sample, language: 'en', user: User.first, lat: "39.907333374", long: "116.391082764")
+Post.create(description: "你好，你过来吧！", capacity: (2..20).to_a.sample, location: Faker::Address.full_address, contact_number: Faker::PhoneNumber.cell_phone, is_full: [true, false].sample, language: 'zh', user: User.first, lat: "31.11325", long: "121.38206")
+
+post1 = Post.create(description: "Shanghai", capacity: (2..20).to_a.sample, location: "Shanghai Pearl Tower", contact_number: Faker::PhoneNumber.cell_phone, is_full: [true, false].sample, language: 'en', user: User.first, lat: "31.240718842", long: "121.501045227")
+
+post2 = Post.create(description: "Beijing", capacity: (2..20).to_a.sample, location: "Tian An Men", contact_number: Faker::PhoneNumber.cell_phone, is_full: [true, false].sample, language: 'en', user: User.first, lat: "39.907333374", long: "116.391082764")
+
+Comment.create(nickname: Faker::Name.name, comment: Faker::Quote.famous_last_words, language: ['zh', 'en'].sample, post: post1)
+Comment.create(nickname: Faker::Name.name, comment: Faker::Quote.famous_last_words, language: ['zh', 'en'].sample, post: post2)
