714d32f55b78a819900aca8f8f30a9a4577ba29f
Pavel Liser <liser.pavel@gmail.com>
2019-06-15 16:00:10 +0800

    added lat and long columns to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_15_041255) do
+ActiveRecord::Schema.define(version: 2019_06_15_075817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2019_06_15_041255) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "language"
+    t.string "lat"
+    t.string "long"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
