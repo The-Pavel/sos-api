82b650661ac71437e21362c55debd07b5c8edfed
Pavel Liser <liser.pavel@gmail.com>
2019-06-15 12:50:05 +0800

    added method in posts controller to create comment, added a POST posts/:id route for comment creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :add_comment ]
+
   def create
   end
 
@@ -12,5 +14,28 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def add_comment
+    @comment = Comment.create(nickname: User.find(params[:user_id].nickname, comment: params[:comment], post: @post))
+    if @comment.save
+      render :index
+    else
+      render :json => { :errors => @comment.errors.full_messages }
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:user_id, :description, :capacity, :location, :contact_number, :is_full)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:comment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resource :posts
+  post 'posts/:id', action: :add_comment, controller: 'posts', as: 'add_comment'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
